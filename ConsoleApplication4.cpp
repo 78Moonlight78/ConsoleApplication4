8dbb51758725c32a070d142bf3afc0f36aa2ea4a
Moonlight <krupenkod@yandex.ru>
2022-10-02 18:30:18 +0300

    Последнее, надеюсь

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -7,15 +7,23 @@
 
 using namespace std;
 
+//Функция очистки консоли 
+void Clear()
+{
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
 
+//Функция проверки соответствия string unsigned long int
 bool check_UI(string s) {
     for (int i = 0; i < strlen(s.c_str()); i++) {
-        /// <summary>
-        /// /if ((isdigit(s[i]) == 0 && i != 0)||(i == 0 && (s[i] != '+' || isdigit(s[i]) == 0))) {     return 0;}
-
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
         if ((isdigit(s[i]) == 0 && i != 0) || (i == 0 && (s[i] != '+' && isdigit(s[i]) == 0))) {
             cout << s[i];
             return 0;
@@ -26,6 +34,8 @@ bool check_UI(string s) {
 
 }
 
+
+//Функция проверки string float 
 bool correct_float(string s) {
     istringstream iss(s);
     float f;
@@ -36,18 +46,23 @@ bool correct_float(string s) {
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian");// Вывод слов на русском
     int id, count, n = 0, j;
     bool b1, b2;
-    union
+
+    union MyUnion
     {
-        float input; // assumes sizeof(float) == sizeof(int)
+        float input; //sizeof(float) == sizeof(int)
         int   output;
     } data;
+
     string sf = "a", su = "a";
     unsigned long int y;
+
+
     cout << "Введите значение типа float(вввещественное число): ";
-    do {
+   
+    do { // Цикл выполняется пока значение float не будет верным(отсутствие букв и других  символов, кроме '.' и цифр) 
         if (sf != "a") {
             cout << "Неверный ввод, попробуйте еще раз: ";
         }
@@ -55,40 +70,51 @@ int main()
     } while (correct_float(sf) != 1);
     cout << "Верно!\n";
 
-    cout << "Введите значение типа unsigned int(натуральное число): ";
+    cout << "Введите значение типа unsigned long int(натуральное число): ";
 
-    do {
+    do { // Цикл выполняется пока значение unsigned long int не будет верным(отсутствие букв и других символов, кроме знака + на первом месте)
         if (su != "a") {
             cout << "Неверный ввод, попробуйте еще раз: ";
         }
         cin >> su;
     } while (check_UI(su) != 1);
     cout << "Верно!" << endl;
-    y = stoul(su);
-    data.input = stof(sf);
-    while (n != 4) {
+    y = stoul(su);//Преобразование string в unsigned long int
+    data.input = stof(sf);//Преобразование string во Float 
+    
+    while (n != 6) {
         cout << "0: Вывести битовые значения числа float\n";
-        cout << "1: Вывести битовые значения числа unsigned int\n";
+        cout << "1: Вывести битовые значения числа unsigned long int\n";
         cout << "2: Поменять местами пары битов в float\n";
-        cout << "3: Поменять местами пары битов в unsugned int\n";
-        cout << "4: Завершить программу\n" << "Ваше решение:";
+        cout << "3: Поменять местами пары битов в unsigned long int\n";
+        cout << "4: Ввести значение float\n";
+        cout << "5: Ввести значение unsigned long int \n";
+        cout << "6: Завершить программу\n" << "Ваше решение:";
         cin >> n;
         if (!cin) {
+            Clear();
             cout << "Ошибка! Введено не число, попробуйте еще раз." << endl;
             cin.clear(); // Снятие ошибки
             cin.get();
-            n = 5;
+            n = 7;
         }
-        while (n < 0 && n>4) {
+        while (n < 0 && n>6) {
             cout << "Неверно! попробуйте еще раз: ";
             cin >> n;
+            if (!cin) {
+                cout << "Ошибка! Введено не число, попробуйте еще раз." << endl;
+                cin.clear(); // Снятие ошибки
+                cin.get();
+            }
         }
         if (n == 0) {
+            Clear();
             //метод по преобразованию float в двоичное предстваление
             bitset<sizeof(float)* CHAR_BIT> bits(data.output);
             cout << "Значение float: " << bits << "\n";
         }
         else if (n == 1) {
+            Clear();
             //метод по преобразованию unsigned long  в двоичное предстваление
             bitset<sizeof(y)* CHAR_BIT> a1(y);
             cout << "Значение unsigned int: " << a1 << endl;
@@ -101,7 +127,7 @@ int main()
                 cin.clear(); // Снимает ошибку
                 cin.get();
             }
-            while (id > 31 || id < 1) {
+            while (id > 31 || id < 1) { // Проверка, что номер элемента лежит в соответсвующем диапазоне. Нумерация начинается справа и с нуля 
                 cout << "Попробуйте еще раз! Введитe номер элемента: ";
                 cin >> id;
                 if (!cin) {
@@ -117,8 +143,8 @@ int main()
                 cin.clear(); // Снимает ошибку
                 cin.get();
             }
-            while (id / 2 + id % 2 < count) {
-                cout << "Попробуйте еще раз! Введитe количество пар: ";
+            while (id / 2 + id % 2 < count && count > 0) { // Проверка введенного количества пар на соответствие 
+                cout << "Попробуйте еще раз!Максимальное количество пар "<<( id / 2 + id % 2)<<". Введитe количество пар: ";
                 cin >> count;
                 if (!cin) {
                     cout << "Ошибка! Введено не число, попробуйте еще раз." << endl;
@@ -126,17 +152,19 @@ int main()
                     cin.get();
                 }
             }
+            Clear();
             bitset<sizeof(float)* CHAR_BIT> bits(data.output);
             cout << "Значение float до преобразований:    " << bits << endl;
-
+            //цикл перестановки пар
             for (int i = id; i > id - count * 2; i -= 2) {
                 b1 = bits[i];
                 j = i - 1;
                 b2 = bits[j];
-                bits[j] = b1;
+                bits[j] = b1;//значение битов меняются местами 
                 bits[i] = b2;
             }
-      
+     
+            data.output = bits.to_ullong();
             cout << "Значение float после преобразований: " << bits << endl;
 
         }
@@ -148,7 +176,7 @@ int main()
                 cin.clear(); // Снимает ошибку
                 cin.get();
             }
-            while (id > 31 || id < 1) {
+            while (id > 31 || id < 1) { // Проверка, что номер элемента лежит в соответсвующем диапазоне. Нумерация начинается справа и с нуля 
                 cout << "Попробуйте еще раз! Введитe номер элемента: ";
                 cin >> id;
                 if (!cin) {
@@ -164,8 +192,8 @@ int main()
                 cin.clear(); // Снимает ошибку
                 cin.get();
             }
-            while (id / 2 + id % 2 < count) {
-                cout << "Попробуйте еще раз! Введитe количество пар: ";
+            while (id / 2 + id % 2 < count) {// Проверка соответсвия введенного количества пар
+                cout << "Попробуйте еще раз!Максимальное количество пар: "<< (id / 2 + id % 2) <<". Введитe количество пар: ";
                 cin >> count;
                 if (!cin) {
                     cout << "Ошибка! Введено не число, попробуйте еще раз." << endl;
@@ -173,19 +201,42 @@ int main()
                     cin.get();
                 }
             }
+            Clear();
             bitset<sizeof(y)* CHAR_BIT> a1(y);
-            cout << "Значение unsigned int до преобразований:    " << a1 << endl;
+            cout << "Значение unsigned long int до преобразований:    " << a1 << endl;
 
             for (int i = id; i > id - count * 2; i -= 2) {
                 b1 = a1[i];
                 j = i - 1;
                 b2 = a1[j];
-                a1[j] = b1;
+                a1[j] = b1;//Замена битов в парах 
                 a1[i] = b2;
             }
-            cout << "Значение unsigned int после преобразований: " << a1 << endl;
+            y = (unsigned long int)(a1.to_ulong());
+            cout << "Значение unsigned long int после преобразований: " << a1 << endl;
 
         }
+        else if (n == 4) {
+            cout << "Введите значение типа float(вввещественное число): ";
+            do { // Цикл выполняется пока значение float не будет верным(отсутствие букв и других  символов, кроме '.' и цифр) 
+                if (sf != "a") {
+                    cout << "Неверный ввод, попробуйте еще раз: ";
+                }
+                cin >> sf;
+            } while (correct_float(sf) != 1);
+            cout << "Верно!\n";
+            data.input = stof(sf);//Преобразование string во Float 
+        }
+         else if (n == 5) {
+            do { // Цикл выполняется пока значение unsigned long int не будет верным(отсутствие букв и других символов, кроме знака + на первом месте)
+                if (su != "a") {
+                    cout << "Неверный ввод, попробуйте еще раз: ";
+                }
+                cin >> su;
+            } while (check_UI(su) != 1);
+            cout << "Верно!" << endl;
+            y = stoul(su);//Преобразование string в unsigned long int
+         }
     }
 
     return 0;
